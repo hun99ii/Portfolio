f1a8e495eb716312b64bc0622091308d98b349e2
Joon Sik Yang <joonsikyang@Joonui-MacBookAir.local>
2022-02-18 16:13:49 +0900

    practice

--- about.js
+++ about.js
@@ -1,32 +1,27 @@
-
 const sunset = document.getElementById("sunset");
 const beach = document.getElementById("beach");
 const closeS = document.getElementById("closeSunset");
 const closeB = document.getElementById("closeBeach");
 
 function handleSun() {
-    const sunPic = document.querySelector(".sunsetPic");
-    if (sunPic.classList.contains("hidden")){
-        sunPic.classList.remove("hidden");
-    
-    } else {
-        sunPic.classList.add("hidden");
-    }
+  const sunPic = document.querySelector(".sunsetPic");
+  if (sunPic.classList.contains("hidden")) {
+    sunPic.classList.remove("hidden");
+  } else {
+    sunPic.classList.add("hidden");
+  }
 }
 
 function handleBeach() {
-    const beachPic = document.querySelector(".beachPic");
-    if (beachPic.classList.contains("hidden")) {
-        beachPic.classList.remove("hidden");
-    } else {
-        beachPic.classList.add("hidden");
-    }
+  const beachPic = document.querySelector(".beachPic");
+  if (beachPic.classList.contains("hidden")) {
+    beachPic.classList.remove("hidden");
+  } else {
+    beachPic.classList.add("hidden");
+  }
 }
 
 sunset.addEventListener("click", handleSun);
 closeS.addEventListener("click", handleSun);
 beach.addEventListener("click", handleBeach);
 closeB.addEventListener("click", handleBeach);
-
- 
-
